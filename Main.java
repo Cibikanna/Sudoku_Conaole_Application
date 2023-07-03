0251e43b4d5dc4265019c2ce0cc17b6e90eaed4a
cibi <kannacibi@gmail.com>
2023-07-03 18:44:22 +0530

    Trojen horse

--- Main.java
+++ Main.java
@@ -1,9 +1,111 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Placing_numbers {
     static int n = 9;
 
-    static void Print(int a[][]) {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Win_or_not win_check = new Win_or_not();
+        Printing prt = new Printing();
+        int board[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 0;
+            }
+        }
+        prt.Print(board, n);
+        board = place(board);
+        System.out.println();
+        prt.Print(board, n);
+        while (true) {
+            System.out.println("Enter the row,coloumn and the number");
+            int row = input.nextInt(), col = input.nextInt(), num = input.nextInt();
+            if (check(board, row, col, num)) {
+                board[row][col] = num;
+                prt.Print(board, n);
+                if (win_check.wincheck(board, n)) {
+                    break;
+                }
+            } else {
+                System.out.println("Kindly check the number it's in valid on this position");
+                continue;
+            }
+        }
+
+    }
+}
+
+class Win_or_not {
+    static boolean wincheck(int board[][], int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Placing_numbers {
+    static int n = 9;
+
+    static boolean check(int a[][], int row, int col, int num) {
+        for (int i = 0; i < n; i++) {
+            if (a[i][col] == num)
+                return false;
+        }
+        for (int i = 0; i < n; i++) {
+            if (a[row][i] == num)
+                return false;
+        }
+        int r = give(row);
+        int c = give(col);
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                if (a[i][j] == num)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    static int give(int a) {
+        if (a >= 0 && a <= 2)
+            return 0;
+        else if (a >= 3 && a <= 5)
+            return 3;
+        else if (a >= 6 && a <= 8)
+            return 6;
+        return 0;
+    }
+
+    static int[][] place(int board[][]) {
+        int h = 0;
+        for (int i = 0; i < n; i++) {
+            if (h >= 9)
+                h = 0;
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    int row = (int) (Math.random() * 9);
+                    int col = (int) (Math.random() * 9);
+                    int num = (int) (Math.random() * 9);
+                    // System.out.println(row + " " + col + h + " " + num);
+                    if (check(board, row, col, num)) {
+                        board[row][col] = num;
+                    }
+
+                }
+            }
+            h += 3;
+        }
+        return board;
+    }
+}
+
+class Printing {
+    static void Print(int a[][], int n) {
         System.out.print("  ");
         for (int j = 0; j < n; j++) {
             if ((j + 1) % 3 == 0)
@@ -29,97 +131,5 @@ public class Main {
             if (i == 2 || i == 5 || i == 8)
                 System.out.println("  - - - - - - - - - - - - - ");
         }
-    }
-
-    static int give(int a) {
-        if (a >= 0 && a <= 2)
-            return 0;
-        else if (a >= 3 && a <= 5)
-            return 3;
-        else if (a >= 6 && a <= 8)
-            return 6;
-        return 0;
-    }
-
-    static boolean check(int a[][], int row, int col, int num) {
-        for (int i = 0; i < n; i++) {
-            if (a[i][col] == num)
-                return false;
-        }
-        for (int i = 0; i < n; i++) {
-            if (a[row][i] == num)
-                return false;
-        }
-        int r = give(row);
-        int c = give(col);
-        for (int i = r; i < r + 3; i++) {
-            for (int j = c; j < c + 3; j++) {
-                if (a[i][j] == num)
-                    return false;
-            }
-        }
-
-        return true;
-    }
-
-    static int[][] place(int board[][]) {
-        int h = 0;
-        for (int i = 0; i < n * n; i++) {
-            if (h >= 9)
-                h = 0;
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
-                    int row = (int) (Math.random() * 9);
-                    int col = (int) (Math.random() * 9);
-                    int num = (int) (Math.random() * 9);
-                    // System.out.println(row + " " + col + h + " " + num);
-                    if (check(board, row, col, num)) {
-                        board[row][col] = num;
-                    }
-
-                }
-            }
-            h += 3;
-        }
-        return board;
-    }
-
-    static boolean wincheck(int board[][]) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 0)
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int board[][] = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = 0;
-            }
-        }
-        Print(board);
-        board = place(board);
-        System.out.println();
-        Print(board);
-        while (true) {
-            System.out.println("Enter the row,coloumn and the number");
-            int row = input.nextInt(), col = input.nextInt(), num = input.nextInt();
-            if (check(board, row, col, num)) {
-                board[row][col] = num;
-                Print(board);
-                if (wincheck(board)) {
-                    break;
-                }
-            } else {
-                System.out.println("Kindly check the number it's in valid on this position");
-                continue;
-            }
-        }
-
     }
 }
